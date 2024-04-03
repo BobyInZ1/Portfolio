6b16241c765293ca5c118ac0a9d715a2f1303890
SossoLeSeau <95532193+BobyInZ1@users.noreply.github.com>
2024-04-03 21:07:25 +0200

    Refonte

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-function CopyToClipboard(mail)
-{
-var r = document.createRange();
-r.selectNode(document.getElementById(mail));
-window.getSelection().removeAllRanges();
-window.getSelection().addRange(r);
-document.execCommand('copy');
-window.getSelection().removeAllRanges();
+function CopyToClipboard(mail) {
+    var r = document.createRange();
+    r.selectNode(document.getElementById(mail));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const degreeToRadian = (angle) => {
+        return angle * (Math.PI / 180);
+    };
+
+    const radius = 200;
+    const diameter = radius * 2;
+
+    const circle = document.querySelector("#circle");
+    circle.style.width = `${diameter}px`;
+    circle.style.height = `${diameter}px`;
+
+    const text = circle.dataset.text;
+    const characters = text.split("");
+
+    const deltaAngle = 360 / characters.length;
+    const characterOffsetAngle = 1;
+    let currentAngle = -90;
+
+    characters.forEach((character, index) => {
+        const span = document.createElement("span");
+        span.innerText = character;
+        const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+        const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+        const transform = `translate(${xPos}px, ${yPos}px)`;
+        const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+        span.style.transform = `${transform} ${rotate}`;
+
+        currentAngle += deltaAngle;
+        circle.appendChild(span);
+    });
+});
---
+++ projets.php
@@ -0,0 +1,202 @@
+<!Doctype html>
+<!--Soan Morey-->
+<html lang="fr">
+
+<head>
+    <link rel="apple-touch-icon" sizes="180x180" href="./Logo/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./Logo/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./Logo/favicon-16x16.png">
+    <link rel="manifest" href="./Logo/site.webmanifest">
+    <link rel="mask-icon" href="./Logo/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title>Soan Morey - Portfolio</title>
+    <meta charset=" UTF-8">
+    <link rel="stylesheet" href="index.css" />
+    <meta name="viewport">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Caption&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="fond">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div class="mainsep">
+        <?php include 'header.php'; ?>
+        <h2>Compétences</h2>
+        <div class="main-division rowlist">
+            <div class="progressblock progressblock1 effetverre">
+                <span class="roundelem1"><img class="stat" src="./IMG/DaVinci_Resolve_17_logo.svg.png"></span>
+                <p class="progresstitle">DaVinci Resolve</p>
+                <p class="progressdescription">Utilisation du logiciel de montage <span class="gras">DaVinci Resolve
+                        Studio</span> dans le
+                    cadre personnel, étudiant et professionnel. </p>
+            </div>
+            <div class="progressblock progressblock2 effetverre">
+                <span class="roundelem2"><img class="stat" src="./IMG/Affinity_Photo_icon.png"></span>
+                <p class="progresstitle">Suite Affinity</p>
+                <p class="progressdescription">Prise en main des logiciels <span class="gras">Affinity Photo,
+                        Designer et Publisher</span>
+                    dans le cadre de TP ou d'essais personnels.</p>
+            </div>
+            <div class="progressblock progressblock3 effetverre">
+                <span class="roundelem3"><img class="stathtml" src="./IMG/kisspng-website-development.png"></span>
+                <p class="progresstitle">Intégration Web</p>
+                <p class="progressdescription">Création de sites web à partir d'<span class="gras">HTML, CSS et
+                        Javascript.</span></p>
+            </div>
+            <div class="progressblock progressblock4 effetverre">
+                <span class="roundelem4"><img class="stat" src="./IMG/figma.png"></span>
+                <p class="progresstitle">Figma</p>
+                <p class="progressdescription">Utilisation de <span class="gras">Figma</span> pour le <span
+                        class="gras">maquettage web</span>
+                    ainsi que pour la réalisation de <span class="gras">visuels</span> simples. Utilisation de la
+                    fonction <span class="gras">"flow"</span> pour le prototypage d'application.</p>
+            </div>
+            <div class="progressblock progressblock1 effetverre">
+                <span class="roundelem5"><img class="stat" src="./IMG/sketch.png"></span>
+                <p class="progresstitle">Sketchbook</p>
+                <p class="progressdescription">Réalisation de <span class="gras">dessins</span> et d'<span
+                        class="gras">animations</span> pour la vidéo promotionelle d'une <span
+                        class="gras">entreprise</span> ainsi que dans le cadre personnel. </p>
+            </div>
+            <div class="progressblock progressblock2 effetverre">
+                <span class="roundelem6"><img class="stat" src="./IMG/images.png"></span>
+                <p class="progresstitle">Premiere Pro</p>
+                <p class="progressdescription" id="division2">Montage de vidéos sur Premiere Pro ainsi que sur <span
+                        class="gras">After Effects</span> pour la réalisation de vidéos dans le cadre personnel.</p>
+            </div>
+            <div class="main-division columncontainer">
+                <div class="rowlist">
+                    <div class="choose-article"><button onclick=window.location.href="#article1">Dessin
+                            isométrique</button></div>
+                    <div class="choose-article"><button onclick=window.location.href="#article2">Tutoriel vidéo
+                        </button>
+                    </div>
+                    <div class="choose-article"><button onclick=window.location.href="#article3">Application
+                            web</button>
+                    </div>
+                    <div class="choose-article"><button onclick=window.location.href="#article4">Site internet </button>
+                    </div>
+                    <div class="choose-article"><button onclick=window.location.href="#article5">Autoportrait
+                            graphique</button>
+                    </div>
+                </div>
+                <div class="difarticles">
+                    <div class="text-article" id="article1">
+                        <img class="iso" src="./IMG/photo_5888666326690544073_w.jpg">
+                        <div class="desstitre">
+                            <p>
+                            <h3>Dessin isométrique</h3>
+                            Dans le cadre d'un TP de culture artistique
+                            Le dessin représente un petit espace carré au centre représentant
+                            un salon. On y voit un canapé, une lampe, un tableau minimaliste,
+                            une étagère a livres, et un bureau sur lequel est posé un clavier, une souris, une tasse et
+                            un écran qui est en fait la fenêtre. Dans le jardin, on peut voir une petite table basse
+                            sur laquelle est posée un pot avec une pousse de bambou ainsi qu’un paillasson. Le jardin
+                            est délimité par une haie et une petite barrière. Derrière la maison, la ville est
+                            représentée par deux immeubles gris collés. L’objectif est ici de montrer le contraste de
+                            couleurs
+                            entre la ville, monotone et la maison, où il y a bien plus de couleurs et d’objets. La seule
+                            ouverture sur la ville est d’ailleurs l’écran/fenêtre.
+                            <br> L'objectif de ce projet était de découvrir et de prendre en main le logiciel "Affinity
+                            Designer" ainsi que de développer sa créativité tout en restant dans la contrainte du dessin
+                            isométrique.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="text-article" id="article2">
+                        <iframe width="4500" height="400" src="https://www.youtube.com/embed/tdtilTQnrGQ"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                        <div class="desstitre">
+                            <p>
+                            <h3>Tutoriel Vidéo</h3>
+                            Réalisation d'une vidéo qui explique la mise en place et le fonctionnement du spot
+                            d'éclairage Amaran 100x.
+                            <br>
+                            Ecriture du script en groupe, tournage des plans à l'aide du Canon 90D, enregistrement de la
+                            voix off en studio et
+                            Montage sur DaVinci Resolve. Ce projet a été réalisé en groupe et à donc nécessité une
+                            certaine organisation en ce qui concerne la répartition des tâches ainsi que pour le
+                            tournage.
+                            L'écriture a aussi demandé un "brainstorm" préalable de sorte a pouvoir organiser nos idées.
+                            <br>
+                            (Sous-titres et mise en ligne par Kelvin Huchet)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="text-article" id="article3">
+                        <img class="web" src="./IMG/webapp.png">
+                        <img class="web" src="./IMG/webapp2.png">
+                        <div class="desstitre">
+                            <p>
+                            <h3>Application web en anglais</h3>
+                            Mise en place d'une parodie de l'application "Messenger" dans le
+                            cadre d'un projet
+                            d'anglais d'autoprésentation. Cette parodie de l'interface utilisateur a été entièrement
+                            réalisée sur Figma.
+                            <br>
+                            Ce projet m'a permis de prendre en main le logiciel Figma ainsi que le prototypage
+                            d'application.
+                            J'ai pu apprendre a utiliser les différentes fonctionnalités de figma et de ses extensions,
+                            tout en rédigeant
+                            le contenu en anglais.
+                            <br>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="text-article2" id="article4">
+                        <div class="rowcontainer">
+                            <img class="web2" src="./IMG/poke.png">
+                            <img class="web2" src="./IMG/poke2.png">
+                            <img class="web2" src="./IMG/poke3.png">
+                        </div>
+                        <div class="desstitre2">
+                            <p>
+                            <h3>Site web, gestion d'une base de données</h3>
+                            Développement et mise en ligne d'un site web
+                            de gestion de collection de Pokémons
+                            Wireframe et flow réalisé sur Figma, front-end du site web
+                            codé en HTML et CSS et back-end en PHP.
+                            <br>
+                            L'objectif de ce projet était de coder "from scratch" un site web dont la base de donnée
+                            (mySQL) était fournie.
+                            Le site devait être capable de créer une collection personnelle sur les demandes de
+                            l'utilisateur qui pouvait donc ajouter et retirer les pokemons
+                            qu'il souhaitait (parmi les 200 disponibles) ainsi qu'afficher les caractéristiques de
+                            chacun.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="text-article" id="article5">
+                        <img class="iso" src="./IMG/auto.jpg">
+                        <div class="desstitre">
+                            <p>
+                            <h3>Autoportrait Graphique</h3>
+                            Assemblage de différentes photos (prises à l'aide du Canon 90D) sur le logiciel Affinity
+                            Photo de sorte a créer un effet de "miroir cassé" dans lesquels chaque fragment est une
+                            photo différente de mon visage et crée un certain décalage.
+                            <br>
+                            L'objectif de ce projet était de créer un autoportrait graphique délivrant un certain
+                            message à l'aide
+                            de photos de soi (prises en TP) que l'on devait modifier tout en
+                            gardant une certaine cohérence. Ce projet m'a permis de perfectionner ma maitrîse d'Affinity
+                            Photo ainsi que de développer
+                            ma créativité.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>

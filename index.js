fe1b352817444031be7043e4c3e740176446ff72
SossoLeSeau <95532193+BobyInZ1@users.noreply.github.com>
2025-06-16 17:27:06 +0200

    final push

--- index.js
+++ index.js
@@ -9,37 +9,42 @@ function CopyToClipboard(mail) {
     x.className = "show";
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
-document.addEventListener("DOMContentLoaded", function () {
-    const degreeToRadian = (angle) => {
-        return angle * (Math.PI / 180);
-    };
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Circle text code
+    const degreeToRadian = (angle) => angle * (Math.PI / 180);
     const radius = 200;
     const diameter = radius * 2;
-
     const circle = document.querySelector("#circle");
-    circle.style.width = `${diameter}px`;
-    circle.style.height = `${diameter}px`;
+    if (circle) {
+        circle.style.width = `${diameter}px`;
+        circle.style.height = `${diameter}px`;
+        const text = circle.dataset.text;
+        const characters = text.split("");
+        const deltaAngle = 360 / characters.length;
+        const characterOffsetAngle = 1;
+        let currentAngle = -90;
+        characters.forEach((character, index) => {
+            const span = document.createElement("span");
+            span.innerText = character;
+            const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+            const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+            const transform = `translate(${xPos}px, ${yPos}px)`;
+            const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+            span.style.transform = `${transform} ${rotate}`;
+            currentAngle += deltaAngle;
+            circle.appendChild(span);
+        });
+    }
 
-    const text = circle.dataset.text;
-    const characters = text.split("");
-
-    const deltaAngle = 360 / characters.length;
-    const characterOffsetAngle = 1;
-    let currentAngle = -90;
-
-    characters.forEach((character, index) => {
-        const span = document.createElement("span");
-        span.innerText = character;
-        const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
-        const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
-
-        const transform = `translate(${xPos}px, ${yPos}px)`;
-        const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
-        span.style.transform = `${transform} ${rotate}`;
-
-        currentAngle += deltaAngle;
-        circle.appendChild(span);
+    // Dropdown for all wrappers
+    document.querySelectorAll('.wrapper').forEach(function(wrapper) {
+        const clickText = wrapper.querySelector('.click-text');
+        if (clickText) {
+            clickText.addEventListener('click', function (e) {
+                e.stopPropagation();
+                wrapper.classList.toggle('active');
+            });
+        }
     });
-
-    });
+});
